57894515aadff5949b535130f2bac5fa0004de9b
Alvaro <alvaroenrique020@gmail.com>
2021-05-15 10:28:12 -0500

    context htttp

--- http.go
+++ http.go
@@ -1,8 +1,10 @@
 package push_notification
 
 import (
+	"cloud.google.com/go/functions/metadata"
 	"dinamo.app/push_notification/response"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,9 +15,18 @@ func HandlerSendPushNotificaction(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	ctx := r.Context()
+	//
+	meta, err := metadata.FromContext(ctx)
+	if err != nil {
+		fmt.Println("error: ",err)
+	}
+	fmt.Println(meta)
+	fmt.Println(ctx)
+	fmt.Println(ctx.Value("auth"))
+	//
 	data := DataNotification{}
 
-	err := json.NewDecoder(r.Body).Decode(&data)
+	err = json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
 		log.Println(err.Error())
 		_ = response.HTTPError(w, r, http.StatusBadRequest, err.Error())
